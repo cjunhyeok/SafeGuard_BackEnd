59b8476e271809a8a91082bf6846cf404011a7be
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-07-31 19:07:22 +0900

    Create BaseTimeEntity and Auditing config for Entity

---
+++ src/main/java/com/opensw/safeguard/domain/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package com.opensw.safeguard.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    // 등록일, 수정일을 공용으로 맵핑하기 위한 엔티티
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate; // 등록일
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate; // 수정일
+}
