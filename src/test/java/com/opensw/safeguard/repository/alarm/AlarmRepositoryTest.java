a4a7b84611cb04e692cc8153cbd7e29bd7adbe6e
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-08-03 22:00:22 +0900

    Add findById Test in AlarmRepositoryTest

--- src/test/java/com/opensw/safeguard/repository/alarm/AlarmRepositoryTest.java
+++ src/test/java/com/opensw/safeguard/repository/alarm/AlarmRepositoryTest.java
@@ -2,14 +2,11 @@ package com.opensw.safeguard.repository.alarm;
 
 import com.opensw.safeguard.domain.Alarm;
 import com.opensw.safeguard.domain.AlarmType;
-import com.opensw.safeguard.domain.Member;
-import com.opensw.safeguard.repository.member.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,26 +16,14 @@ class AlarmRepositoryTest {
 
     @Autowired
     private AlarmRepository alarmRepository;
-    @Autowired
-    private MemberRepository memberRepository;
 
     @Test
+    @DisplayName("알림 리포지토리 저장 테스트")
     void saveTest() {
         // given
-        ArrayList<String> roles = new ArrayList<>();
-        roles.add("ROLE_USER");
-        Member member = Member.builder()
-                .username("id")
-                .password("password")
-                .roles(roles)
-                .email("asc@naver.com")
-                .build();
-        memberRepository.save(member);
-
         Alarm alarm = Alarm.builder()
                 .message("테스트 알림입니다.")
                 .alarmType(AlarmType.WARN)
-                .member(member)
                 .build();
 
         // when
@@ -46,6 +31,27 @@ class AlarmRepositoryTest {
 
         // then
         assertThat(savedAlarm.getIsRead()).isFalse();
+        assertThat(savedAlarm.getMessage()).isEqualTo("테스트 알림입니다.");
+        assertThat(savedAlarm.getAlarmType()).isEqualTo(AlarmType.WARN);
+    }
+
+    @Test
+    @DisplayName("알림 리포지토리 PK 조회 테스트")
+    void findByIdTest() {
+        // given
+        Alarm alarm = Alarm.builder()
+                .message("테스트 알림입니다.")
+                .alarmType(AlarmType.WARN)
+                .build();
+        Alarm savedAlarm = alarmRepository.save(alarm);
+
+        // when
+        Alarm findAlarm = alarmRepository.findById(savedAlarm.getId()).orElseThrow(
+                () -> new IllegalArgumentException()
+        );
+
+        // then
+        assertThat(findAlarm).isEqualTo(savedAlarm);
     }
 
 }
