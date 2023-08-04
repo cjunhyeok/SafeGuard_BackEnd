e21adb81579fa5fa8ef3abbb1dc1e2397453047b
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-08-04 12:08:45 +0900

    Refactor AlarmService add Member Association

--- src/main/java/com/opensw/safeguard/service/alarm/AlarmServiceImpl.java
+++ src/main/java/com/opensw/safeguard/service/alarm/AlarmServiceImpl.java
@@ -2,7 +2,9 @@ package com.opensw.safeguard.service.alarm;
 
 import com.opensw.safeguard.domain.Alarm;
 import com.opensw.safeguard.domain.AlarmType;
+import com.opensw.safeguard.domain.Member;
 import com.opensw.safeguard.repository.alarm.AlarmRepository;
+import com.opensw.safeguard.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,13 +17,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class AlarmServiceImpl implements AlarmService{
 
     private AlarmRepository alarmRepository;
+    private MemberRepository memberRepository;
 
     @Override
     @Transactional
-    public Long saveAlarm(String message, AlarmType alarmType) {
+    public Long saveAlarm(String message, AlarmType alarmType, Long memberId) {
+        Member findMember = memberRepository.findById(memberId).orElseThrow(
+                () -> new IllegalArgumentException("Member Not exist")
+        );
+
         Alarm alarm = Alarm.builder()
                 .message(message)
                 .alarmType(alarmType)
+                .member(findMember)
                 .build();
 
         Alarm savedAlarm = alarmRepository.save(alarm);
