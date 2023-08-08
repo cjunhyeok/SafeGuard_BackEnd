20a3705a31612f61d9003fb401ddfa8eb9efa3b1
gosu <q63530@naver.com>
2023-08-08 20:02:59 +0900

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/opensw/safeguard/service/join/MemberJoinService.java
+++ src/main/java/com/opensw/safeguard/service/join/MemberJoinService.java
@@ -2,38 +2,11 @@ package com.opensw.safeguard.service.join;
 
 import com.opensw.safeguard.domain.Member;
 import com.opensw.safeguard.domain.dto.DuplicateUsername;
-import com.opensw.safeguard.repository.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import com.opensw.safeguard.security.token.TokenInfo;
 
-import java.util.List;
+public interface MemberJoinService {
+    public Member join(String memberId, String password , String email);
+    public DuplicateUsername duplicateCheckUsername(String username);
 
-@Service
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
-public class MemberJoinService {
-    private final MemberRepository memberRepository;
-    @Transactional
-    public Member join(String memberId, String password , String email){
-        Member member = Member.builder()
-                .username(memberId)
-                .password(password)
-                .email(email)
-                .roles(List.of("USER"))
-                .build();
-        return memberRepository.save(member);
 
-    }
-
-    @Transactional
-    public DuplicateUsername duplicateCheckUsername(String username){
-        DuplicateUsername duplicateUsername = DuplicateUsername.
-                builder()
-                .username(username)
-                .duplicate(memberRepository.existsByUsername(username))
-                .build();
-        return duplicateUsername;
-
-    }
 }
