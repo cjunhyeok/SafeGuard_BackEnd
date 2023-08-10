bf491a6c374ad4b48c3b3ee650b9807ff17af121
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-08-10 11:28:21 +0900

    Create EmitterApiController with subscribe Method

---
+++ src/main/java/com/opensw/safeguard/controller/alarm/EmitterApiController.java
@@ -0,0 +1,30 @@
+package com.opensw.safeguard.controller.alarm;
+
+import com.opensw.safeguard.domain.Member;
+import com.opensw.safeguard.security.service.MemberContext;
+import com.opensw.safeguard.service.alarm.EmitterService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class EmitterApiController {
+
+    private final EmitterService emitterService;
+
+    @GetMapping(value = "/api/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe(@RequestHeader(value = "Last-Event-Id", required = false, defaultValue = " ") String lastEventId,
+                                @AuthenticationPrincipal MemberContext memberContext) {
+
+        Member loginMember = memberContext.getMember();
+
+        return emitterService.subscribe(loginMember.getId());
+    }
+}
