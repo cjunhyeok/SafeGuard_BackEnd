30dcc972003abe095fb6f8658be855ab6a24b22e
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-08-11 10:09:51 +0900

    Refactor EmitterApiController subscribe Parameter MemberContext to UserDetails

--- src/main/java/com/opensw/safeguard/controller/alarm/EmitterApiController.java
+++ src/main/java/com/opensw/safeguard/controller/alarm/EmitterApiController.java
@@ -1,12 +1,13 @@
 package com.opensw.safeguard.controller.alarm;
 
 import com.opensw.safeguard.domain.Member;
-import com.opensw.safeguard.security.service.MemberContext;
 import com.opensw.safeguard.service.alarm.EmitterService;
+import com.opensw.safeguard.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +19,14 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class EmitterApiController {
 
     private final EmitterService emitterService;
+    private final MemberService memberService;
 
     @GetMapping(value = "/api/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe(@RequestHeader(value = "Last-Event-Id", required = false, defaultValue = " ") String lastEventId,
-                                @AuthenticationPrincipal MemberContext memberContext) {
+                                @AuthenticationPrincipal UserDetails userDetails) {
 
-        Member loginMember = memberContext.getMember();
+        String loginUsername = userDetails.getUsername();
+        Member loginMember = memberService.findByUsername(loginUsername);
 
         return emitterService.subscribe(loginMember.getId());
     }
