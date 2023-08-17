8a3016100d6c3e02da9f38dc785a1cb7bce8a9a4
gosu <q63530@naver.com>
2023-08-17 19:28:43 +0900

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/opensw/safeguard/controller/member/MemberController.java
+++ src/main/java/com/opensw/safeguard/controller/member/MemberController.java
@@ -1,47 +1,37 @@
 package com.opensw.safeguard.controller.member;
 
 import com.opensw.safeguard.domain.Member;
-import com.opensw.safeguard.domain.QMember;
 import com.opensw.safeguard.domain.dto.DuplicateUsername;
 import com.opensw.safeguard.domain.dto.EmailConfirmDTO;
 import com.opensw.safeguard.domain.dto.MemberJoinDTO;
 import com.opensw.safeguard.domain.dto.MemberLoginDTO;
 import com.opensw.safeguard.domain.image.ImageUpload;
-import com.opensw.safeguard.domain.image.ImageUploadDTO;
 import com.opensw.safeguard.email.AuthCode;
 import com.opensw.safeguard.email.EmailService;
 import com.opensw.safeguard.security.service.MemberAdapter;
 import com.opensw.safeguard.security.token.TokenInfo;
 import com.opensw.safeguard.service.image.ImageService;
-import com.opensw.safeguard.service.image.ImageServiceImpl;
 import com.opensw.safeguard.service.member.MemberService;
 import jakarta.mail.MessagingException;
-import jakarta.mail.Multipart;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.naming.SizeLimitExceededException;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Optional;
-
-import static com.opensw.safeguard.domain.QMember.member;
 
 
 @RestController
@@ -85,7 +75,10 @@ public class MemberController {
             String storedFileName = image.getStoredFileName();
 
             String path = new File("").getAbsolutePath() + "/" + storedFileName;
-            return ResponseEntity.ok().body(ImageUploadDTO.builder().message("ok").build());
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("message","OK");
+            log.info("json = {}",jsonObject);
+            return ResponseEntity.ok().body(jsonObject.toMap());
 
 
     }
@@ -99,8 +92,7 @@ public class MemberController {
                 String path = new File("").getAbsolutePath() + "/" + image.getStoredFileName();
                 FileSystemResource resource = new FileSystemResource(path);
                 HttpHeaders header = new HttpHeaders();
-                Path filePath = null;
-                filePath = Paths.get(path);
+                Path filePath = Paths.get(path);
                 header.add("Content-Type", Files.probeContentType(filePath));
                 return new ResponseEntity<Resource>(resource, header, HttpStatus.OK);
     }
