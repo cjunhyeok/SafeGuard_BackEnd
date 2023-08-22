dd2a47d8646bc7ea29a4658ce681981c1911e31f
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-08-22 10:42:23 +0900

    Refactor saveDeviceToken parameter MemberAdapter to UserDetails in DeviceTokenController

--- src/main/java/com/opensw/safeguard/controller/devicetoken/DeviceTokenController.java
+++ src/main/java/com/opensw/safeguard/controller/devicetoken/DeviceTokenController.java
@@ -2,12 +2,13 @@ package com.opensw.safeguard.controller.devicetoken;
 
 import com.opensw.safeguard.controller.devicetoken.dto.SaveDeviceTokenRequest;
 import com.opensw.safeguard.domain.Member;
-import com.opensw.safeguard.security.service.MemberAdapter;
 import com.opensw.safeguard.service.devicetoken.DeviceTokenService;
+import com.opensw.safeguard.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class DeviceTokenController {
 
     private final DeviceTokenService deviceTokenService;
+    private final MemberService memberService;
 
     @PostMapping("/api/deviceToken/new")
     public ResponseEntity saveDeviceToken(@RequestBody SaveDeviceTokenRequest request,
-                                          @AuthenticationPrincipal MemberAdapter memberAdapter) {
-        Member loginMember = memberAdapter.getMember();
+                                          @AuthenticationPrincipal UserDetails userDetails) {
+        String loginUsername = userDetails.getUsername();
+        Member loginMember = memberService.findByUsername(loginUsername);
 
         Long savedDeviceTokenId = deviceTokenService.save(request.getDeviceToken(), loginMember.getId());
         return ResponseEntity.ok(savedDeviceTokenId);
