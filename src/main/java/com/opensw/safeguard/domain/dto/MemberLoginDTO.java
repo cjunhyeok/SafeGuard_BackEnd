e720a07842b7ef2615492ce71b8cb4528a91dfba
gosu <q63530@naver.com>
2023-08-27 17:21:59 +0900

    d

--- src/main/java/com/opensw/safeguard/domain/dto/MemberLoginDTO.java
+++ src/main/java/com/opensw/safeguard/domain/dto/MemberLoginDTO.java
@@ -1,11 +1,19 @@
 package com.opensw.safeguard.domain.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.codehaus.jackson.annotate.JsonProperty;
 
 @Getter
 @Setter
+@Schema(description = "memberLoginDTO")
+
 public class MemberLoginDTO {
+
     private String username;
 
     private String password;
